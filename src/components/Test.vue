9ef0bb3e46aad6025f71795c0c852dc1de0c4f4a
Amy Hanvoravongchai <amy.hanv@gmail.com>
2019-01-16 21:59:10 -0800

    Add directives, bindings, events

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="test">
-    <h1>Testing testing</h1>
+    <h1>{{title}}</h1>
+
+    <!-- binding to title-->
+    <input type="text" v-model="title">
+
+    <!-- same as v-text -->
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else="showName">Nobody</p>
+    <!-- <p v-html="user.firstName"></p> -->
+    <!-- loop thru title -->
+    <ul>
+      <li v-for="element in items">{{element.title}}</li>
+    </ul>
+
+    <!-- events -->
+    <button v-on:click="greet">Say greeting</button>
   </div>
 </template>
 
@@ -8,7 +23,24 @@
 
 <script>
 export default {
-  name: "test"
+  name: "test",
+  data() {
+    return {
+      title: "Hello world omfg",
+      user: {
+        firstName: "Amy",
+        lastName: "Hanvoravongchai"
+      },
+      //conditionals
+      showName: true,
+      items: [{ title: "Item 1" }, { title: "Item 2" }, { title: "Item 3" }]
+    };
+  },
+  methods: {
+    greet: function() {
+      alert("What up my dawg?");
+    }
+  }
 };
 </script>
 
