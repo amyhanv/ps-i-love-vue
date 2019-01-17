3faf3a6a1f797479bc8d4ae9db036060dda8d563
Amy Hanvoravongchai <amy.hanv@gmail.com>
2019-01-16 21:33:52 -0800

    Create new test component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <test></test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Test from "./components/test";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Test
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
